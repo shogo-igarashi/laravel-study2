cde63cb8f620f712ff4e7624dafecb35349d3124
igarashi <shogo.igarashi@asia-quest.jp>
2021-06-02 15:58:01 +0900

    create（新規登録フォーム）の作成

--- app/app/Http/Controllers/ContactFormController.php
+++ app/app/Http/Controllers/ContactFormController.php
@@ -20,11 +20,11 @@ class ContactFormController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('contact.create');
     }
 
     /**
--- app/routes/web.php
+++ app/routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', function () {
 //'middleware' => 'authで強制的にログイン画面へ遷移
 Route::group(['prefix' => 'contact'], function(){
     Route::get('index', [ContactFormController::class,'index'])->name('contact.index');
+    Route::get('create', [ContactFormController::class,'create'])->name('contact.create');
+    Route::post('store', [ContactFormController::class,'store'])->name('contact.store');
+
 });
 //REST
 //Route::resource('contacts', 'ContactFormController');
